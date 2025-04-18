588be8f6ec0b8841bec6242be6cb75c984d3d262
Samuel Heid <sjheid@t-online.de>
2025-04-18 11:30:30 +0200

    feat(BaseData): enhance data handling with type validation and JSON serialization

--- src/db/ORM/BaseData.py
+++ src/db/ORM/BaseData.py
@@ -1,41 +1,101 @@
-from db.ORM.ModelMeta import ModelMeta
+import json
+import typing
+
+from db.ORM.ModelMeta import MetaInformation, ModelMeta
 
 
 class BaseData(metaclass=ModelMeta):
     def __init__(self, **kwargs):
-        self._meta: dict = self.__class__._meta  # Zugriff auf die Metadaten der Klasse
+        self._meta: MetaInformation = self.__class__._meta  # Zugriff auf die Metadaten der Klasse
         self._data = {}  # Hier könnten die aktuellen Werte der Instanz gespeichert werden
         self._loaded_from_db = False  # Flag, ob das Objekt aus der DB kommt
 
-        for field_name, field_obj in self._meta['fields'].items():
+        for field_name, field_obj in self._meta.fields.items():
             # Setze Standardwerte oder übergebene Werte
             value = kwargs.get(field_name, field_obj.default_value)
+            if isinstance(value, dict):
+                # Wenn der Wert ein Dictionary ist, konvertiere ihn in das richtige Format
+                value = field_obj.reference.data(**value)
             setattr(self, field_name, value)
+
+        self.validate_types()
 
     def __repr__(self):
         # Gibt eine lesbare Darstellung des Objekts zurück
-        field_values = ', '.join(f"{k}={getattr(self, k)}" for k in self._meta['fields'].keys())
+        field_values = ', '.join(f"{k}={getattr(self, k)}" for k in self._meta.fields.keys())
         return f"{self.__class__.__name__}({field_values})"
 
     def __eq__(self, other: object) -> bool:
         # Vergleicht zwei Objekte basierend auf den Werten der Felder
         if not isinstance(other, self.__class__):
             return False
-        for field_name in self._meta['fields'].keys():
+        for field_name in self._meta.fields.keys():
             if getattr(self, field_name) != getattr(other, field_name):
                 return False
         return True
+
+    def validate_types(self):
+        # check types referenced in the annotations
+
+        for name, expected_type in self.__annotations__.items():
+            origin = typing.get_origin(expected_type)
+            args = typing.get_args(expected_type)
+            value = getattr(self, name)
+
+            if origin is typing.Literal:
+                continue
+            if not all(not isinstance(value, list) for arg in args):
+                continue
+
+            if origin is typing.Union and type(None) in args:  # Optional-Typ
+                non_none_args = tuple(arg for arg in args if arg is not type(None))
+                isvalid = isinstance(value, non_none_args) or value is None
+            elif origin:  # Andere generische Typen
+                isvalid = isinstance(value, origin)
+            else:  # Standardtypen
+                isvalid = isinstance(value, expected_type) or not (
+                        expected_type is type(bool) and isinstance(value, int))
+
+            if not isvalid:
+                raise TypeError(f"Type of {name} is not {expected_type}, but {type(value)}")
+
 
     @property
     def primary_key(self) -> list:
         """
         Returns the primary key of the object.
         """
-        return [getattr(self, field.field_name) for field in self._meta['primary_key_field']]
+        return [getattr(self, field.field_name) for field in self._meta.primary_keys]
 
     @property
-    def meta(self) -> dict:
+    def flattened_primary_key(self) -> tuple:
+        """
+        Returns the primary key of the object as a flattened list.
+        """
+        key = []
+        for k in self.primary_key:
+            if isinstance(k, BaseData):
+                key.extend(k.flattened_primary_key)
+            else:
+                key.append(k)
+
+        return tuple(key)
+
+    @classmethod
+    def meta(cls) -> MetaInformation:
         """
         Returns the meta information of the object.
         """
-        return self._meta
+        return cls._meta
+
+    def json(self) -> str:
+        """
+        Returns a JSON representation of the object.
+        """
+        return json.dumps(self.json_as_dict())
+
+    def json_as_dict(self) -> dict:
+        """
+        Returns a JSON representation of the object as a dictionary.
+        """
+        return {k: getattr(self, k) for k in self._meta.fields.keys()}
