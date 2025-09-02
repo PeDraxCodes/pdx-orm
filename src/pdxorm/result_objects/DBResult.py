6e3fe002b1ec5745bce0cb5aa19a22e179b7fe4e
Samuel Heid <sjheid@t-online.de>
2025-09-02 18:36:13 +0200

    feat: add abstract DBResult class and implement SqliteDBResult

--- src/pdxorm/result_objects/DBResult.py
+++ src/pdxorm/result_objects/DBResult.py
@@ -1,16 +1,16 @@
-import sqlite3
+from abc import ABC, abstractmethod
 from typing import Any
 
 
-class DBResult:
-    def __init__(self, result: sqlite3.Cursor):
-        self._result = result
+class DBResult(ABC):
 
     @property
+    @abstractmethod
     def to_list(self) -> list[tuple]:
-        return self._result.fetchall()
+        ...
 
     @property
+    @abstractmethod
     def to_dict(self) -> list[dict]:
         """
         Converts the SQL query result to a list of dictionaries.
@@ -21,23 +21,23 @@ class DBResult:
         Returns:
             list[dict]: A list of dictionaries with the column names as keys and the values of the row as values.
         """
-        result = self._result.fetchall()
-        return [dict(zip([col[0] for col in self._result.description], row)) for row in result]
+        ...
 
     @property
+    @abstractmethod
     def to_item(self) -> Any | None:
         """
         :returns: None if result is empty
                   else first item of the first row of the result
         """
-        res = self.to_list
-        return None if not res else res[0][0]
+        ...
 
     @property
+    @abstractmethod
     def to_items(self) -> list:
         """
         :returns: empty list if result is empty
                   else list of first items of each row of the result
 
         """
-        return list(map(lambda x: x[0], self.to_list))
+        ...
---
+++ src/pdxorm/result_objects/SqliteDBResult.py
@@ -0,0 +1,45 @@
+import sqlite3
+from typing import Any
+
+from .DBResult import DBResult
+
+
+class SqliteDBResult(DBResult):
+    def __init__(self, result: sqlite3.Cursor):
+        self._result = result
+
+    @property
+    def to_list(self) -> list[tuple]:
+        return self._result.fetchall()
+
+    @property
+    def to_dict(self) -> list[dict]:
+        """
+        Converts the SQL query result to a list of dictionaries.
+
+        Note:
+            The SQL query has to return a struct or row data.
+
+        Returns:
+            list[dict]: A list of dictionaries with the column names as keys and the values of the row as values.
+        """
+        result = self._result.fetchall()
+        return [dict(zip([col[0] for col in self._result.description], row)) for row in result]
+
+    @property
+    def to_item(self) -> Any | None:
+        """
+        :returns: None if result is empty
+                  else first item of the first row of the result
+        """
+        res = self.to_list
+        return None if not res else res[0][0]
+
+    @property
+    def to_items(self) -> list:
+        """
+        :returns: empty list if result is empty
+                  else list of first items of each row of the result
+
+        """
+        return list(map(lambda x: x[0], self.to_list))
